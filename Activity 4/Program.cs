076139f29ae4a7e5af635247047eac70cc50da19
sntgjm <jm2001santiago@gmail.com>
2022-10-23 14:02:06 +0800

    Add project files.

---
+++ Activity 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < numbers.Length; i++)//manual
+            {
+                Console.WriteLine("The number is(Manual Typing) " + numbers[i]);
+            }
+
+        }
+    }
+}
